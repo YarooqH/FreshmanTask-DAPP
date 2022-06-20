fb283698374703c1ca066d24780997dd79a4bfcf
Yarooq Anwar <yarooq1@gmail.com>
2022-06-20 23:32:21 +0500

    Updated

--- NFT-Tutorial/contracts/NFTEE.sol
+++ NFT-Tutorial/contracts/NFTEE.sol
@@ -8,7 +8,6 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract GameItem is ERC721 {
 
     constructor() ERC721("GameItem", "ITM") {
-        // mint an NFT to yourself
         _mint(msg.sender, 1);
     }
 }
